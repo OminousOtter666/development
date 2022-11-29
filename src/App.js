ea0c27d7cced847843187dad5cb5189351ff12f0
OminousOtter666 <115323214+OminousOtter666@users.noreply.github.com>
2022-11-29 16:30:49 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ItemList from "./components/ItemList";
+import list from "./assets/list.json";
+import React from "react";
+import Planner from "./components/Planner";
 
 function App() {
+  const [selected, setSelected] = React.useState([]);
+  const alphaList = list.data;
+  alphaList.sort((a, b) => a.name.localeCompare(b.name));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-body">
+        <div className="page-title">Organization Planner</div>
+        <div className="page-desc">
+          This app is a planner designed for people who need help deciding what tasks they should get done in a day. You can sort/filter items based on various criteria and then view the total estimated hours to complete all items for the day!
+        </div>
+        <ItemList
+          data={alphaList}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      </div>
+      <Planner selected={selected} setSelected={setSelected} />
     </div>
   );
 }
